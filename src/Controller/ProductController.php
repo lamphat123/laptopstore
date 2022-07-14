4b14dfe246ea0fd0ae37dbd73bd6993a1348696d
tranlamdat <dattlgbc200045@fpt.edu.vn>
2022-07-14 22:56:32 +0700

    Edit Product Feature

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -16,28 +16,28 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProductController extends AbstractController
 {
-    /**
-     * @Route("/product", name="app_product")
-     */
-    public function showProductAction(ProductRepository $repo): Response
-    {
-        $product = $repo->findAll();
-        return $this->render('Product/index.html.twig',[
-            'product'=> $product
-        ]);
-    }
-    /**
-     * @Route("/product/show/{id}", name="ProductShow")
-     */
-    public function showView(ProductRepository $repo, $id)
-    {
-        $product = $repo->find($id);
+  /**
+   * @Route("/product", name="app_product")
+   */
+  public function showProductAction(ProductRepository $repo): Response
+  {
+    $product = $repo->findAll();
+    return $this->render('Product/index.html.twig', [
+      'product' => $product
+    ]);
+  }
+  /**
+   * @Route("/product/show/{id}", name="ProductShow")
+   */
+  public function showView(ProductRepository $repo, $id)
+  {
+    $product = $repo->find($id);
 
-        return $this->render('Product/show.html.twig', [
-            'products' => $product
-        ]);
-    }
-    /**
+    return $this->render('Product/show.html.twig', [
+      'products' => $product
+    ]);
+  }
+  /**
    * @Route("/product/add", name="addProduct")
    */
   public function addProductAction(ManagerRegistry $res, Request $req, SluggerInterface $slugger, ValidatorInterface $valid): Response
@@ -46,7 +46,7 @@ class ProductController extends AbstractController
     $productForm = $this->createForm(ProductFormType::class, $product);
     $productForm->handleRequest($req);
     $entity = $res->getManager();
-    if($productForm->isSubmitted() && $productForm->isValid()) {
+    if ($productForm->isSubmitted() && $productForm->isValid()) {
       $data = $productForm->getData();
       $product->setProductName($data->getProductName());
       $product->setPrice($data->getPrice());
@@ -60,24 +60,58 @@ class ProductController extends AbstractController
 
       $imgFile = $productForm->get('image')->getData();
       if ($imgFile) {
-          $originalFilename = pathinfo($imgFile->getClientOriginalName(), PATHINFO_FILENAME);
-          $safeFilename = $slugger->slug($originalFilename);
-          $newFilename = $safeFilename . '-' . uniqid() . '.' . $imgFile->guessExtension();
-          try {
-              $imgFile->move(
-                  $this->getParameter('image_dir'),
-                  $newFilename
-              );
-          } catch (FileException $e) {
-              echo $e;
-          }
-          $product->setImage($newFilename);
+        $originalFilename = pathinfo($imgFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $slugger->slug($originalFilename);
+        $newFilename = $safeFilename . '-' . uniqid() . '.' . $imgFile->guessExtension();
+        try {
+          $imgFile->move(
+            $this->getParameter('image_dir'),
+            $newFilename
+          );
+        } catch (FileException $e) {
+          echo $e;
+        }
+        $product->setImage($newFilename);
       }
       $entity->persist($product);
       $entity->flush();
       return $this->redirectToRoute("app_product");
     }
-    return $this->render('product/add.html.twig', [ 
+    return $this->render('product/add.html.twig', [
+      'form' => $productForm->createView()
+    ]);
+  }
+  /**
+   * @Route("product/edit/{id}", name="editProduct")
+   */
+  public function editProductAction(ManagerRegistry $res, Request $req, ValidatorInterface $valid, SluggerInterface $slugger, ProductRepository $repo, $id): Response
+  {
+    $product = $repo->find($id);
+    $productForm = $this->createForm(ProductFormType::class, $product);
+    $productForm->handleRequest($req);
+    $entity = $res->getManager();
+    if ($productForm->isSubmitted() && $productForm->isValid()) {
+      $data = $productForm->getData();
+      $product->setProductName($data->getProductName());
+      $product->setPrice($data->getPrice());
+      $product->setOldPrice($data->getOldPrice());
+      $product->setSmallDesc($data->getSmallDesc());
+      $product->setDetailDesc($data->getDetailDesc());
+      $product->setProDate($data->getProDate());
+      $product->setProQty($data->getProQty());
+      $product->setSupplier($data->getSupplier());
+      $product->setCategories($data->getCategories());
+      $product->setImage($data->getImage());
+      $err = $valid->validate($product);
+      if (count($err) > 0) {
+        $string_err = (string)$err;
+        return new Response($string_err, 400);
+      }
+      $entity->persist($product);
+      $entity->flush();
+      return $this->redirectToRoute("app_product");
+    }
+    return $this->render('product/add.html.twig', [
       'form' => $productForm->createView()
     ]);
   }
